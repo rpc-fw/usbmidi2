3665d30c9c45a92c02a6dc7a2b6a96b2e7a6e088
Lauri Koponen <lauri.koponen@iki.fi>
2015-09-14 21:22:01 +0200

    Update make.sh for 4x4.

--- ihextools/make.sh
+++ ihextools/make.sh
@@ -13,3 +13,17 @@ hextosyx/hextosyx slave.hex
 
 cp slave.hex.syx usbmidi2.syx
 
+
+
+echo Converting 4x4 master binary
+
+cp ../usbmidi2_4x4_master/Debug/usbmidi2_master.hex master4x4.hex
+BUILDID=$(md5sum master4x4.hex|cut -c -8)
+hextoc/hextoc master4x4.hex -i 0x$BUILDID -P 2400:$(./sense_master_id.sh master4x4.hex):$BUILDID > master4x4.h
+cp master4x4.h ../usbmidi2_4x4_slave/Sources/masterbinary.h
+
+echo Converting 4x4 slave binary
+cp ../usbmidi2_4x4_slave/Debug/usbmidi2_slave.hex slave4x4.hex
+hextosyx/hextosyx slave4x4.hex
+
+cp slave4x4.hex.syx usbmidi2_4x4.syx
